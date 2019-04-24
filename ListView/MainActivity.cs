82b7ecb0bb314112386a0f9ce2f888fa207a7e63
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2019-04-24 20:10:03 +0530

    video 2 form there https://www.youtube.com/watch?v=zZHLHrkvUt0&list=PLCuRg51-gw5VqYchUekCqxUS9hEZkDf6l&index=2

---
+++ ListView/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+using Android.Views;
+using Android.Runtime;
+using Android.Widget;
+
+namespace ListView
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private List<string> mItems;
+        private AbsListView mListView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            mListView = FindViewById<AbsListView>(Resource.Id.myListView);
+
+            mItems = new List<string>();
+            mItems.Add("Himanshu");
+            mItems.Add("James");
+            mItems.Add("Sonu");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleExpandableListItem1, mItems);
+
+            mListView.Adapter = adapter;
+
+        }
+    }
+}
